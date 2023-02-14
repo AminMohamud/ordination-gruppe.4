c091fc7d0c8b4e8c4a92420c78150ee27e2c635a
silasskovchristensen <silasskovchristensen@gmail.com>
2023-02-14 11:32:02 +0100

    ordination ændringer

--- ordination/DagligFast.java
+++ ordination/DagligFast.java
@@ -8,9 +8,17 @@ import java.util.ArrayList;
 public class DagligFast extends Ordination {
 
     private int[] doser = new int[4];
+    private int morgenAntal;
+    private int middagAntal;
+    private int AftensAntal;
+    private int natAntal;
 
     public DagligFast (LocalDate startDato, LocalDate slutDato, Patient patient, int morgenAntal, int middagAntal, int aftenAntal, int natAntal,) {
         super(startDato, slutDato, patient);
+        this.morgenAntal = morgenAntal;
+        this.middagAntal = middagAntal;
+        this.AftensAntal = aftenAntal;
+        this.natAntal = natAntal;
 
     }
     public Dosis opretDosis(LocalTime tid, double antal) {
