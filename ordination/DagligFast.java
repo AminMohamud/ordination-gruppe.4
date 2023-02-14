aaf83739ad72d569a0a5606f4e0b0e1823e1ddbe
Ayyminq <amin.dahir@hotmail.com>
2023-02-14 10:42:45 +0100

    commit

--- ordination/DagligFast.java
+++ ordination/DagligFast.java
@@ -1,5 +1,33 @@
 package ordination;
 
-public class DagligFast {
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class DagligFast extends Ordination {
+
+    ArrayList<Dosis> doser = new ArrayList<>();
+
+    public DagligFast () {
+        super();
+    }
+    public Dosis opretDosis(LocalTime tid, double antal) {
+        Dosis dosis = new Dosis(tid, antal);
+        doser.add(dosis);
+        return dosis;
+    }
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
     // TODO
 }
--- ordination/DagligSkaev.java
+++ ordination/DagligSkaev.java
@@ -1,12 +1,35 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination {
 
+    private ArrayList<Dosis> doser = new ArrayList<>();
 
-    public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+    public DagligSkaev() {
+        super();
+    }
+
+    public Dosis opretDosis(LocalTime tid, double antal) {
+        Dosis dosis = new Dosis(tid, antal);
+        doser.add(dosis);
+        return dosis;
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
