7ec3ad494f62ca026e72b18d2fbfaf4ae6bb166a
silasskovchristensen <silasskovchristensen@gmail.com>
2023-02-15 09:22:48 +0100

    ordination ændringer

--- ordination/DagligSkaev.java
+++ ordination/DagligSkaev.java
@@ -3,14 +3,19 @@ package ordination;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class DagligSkaev extends Ordination {
 
     private ArrayList<Dosis> doser = new ArrayList<>();
+    private LocalDate startDato;
+    private LocalDate slutDato;
 
     public DagligSkaev(LocalDate startDato, LocalDate slutDato, Patient patient) {
         super(startDato, slutDato, patient);
+        this.startDato = startDato;
+        this.slutDato = slutDato;
         doser = new ArrayList<>(doser);
     }
 
@@ -22,12 +27,17 @@ public class DagligSkaev extends Ordination {
 
     @Override
     public double samletDosis() {
-        return 0;
+        long daysbetween = ChronoUnit.DAYS.between(startDato,slutDato);
+        return doegnDosis() * daysbetween;
     }
 
     @Override
     public double doegnDosis() {
-        return 0;
+        double result = 0;
+        for (Dosis dos : doser) {
+            result += dos.getAntal();
+        }
+        return result;
     }
 
     @Override
