59ead5cac9090e994bc168533e317f5830555044
silasskovchristensen <silasskovchristensen@gmail.com>
2023-02-16 09:22:23 +0100

    ordination ændringer

--- JUnit Tests/src/OrdinationTest/PNTest.java
+++ JUnit Tests/src/OrdinationTest/PNTest.java
@@ -18,8 +18,6 @@ class PNTest {
         Patient subject = new Patient("111111-1111", "Anders And", 80.00);
         PN test = new PN(LocalDate.of(1, 2,23), LocalDate.of(12,2,23), 1.00, subject);
         LocalDate givenDate = LocalDate.of(2,2,23);
-        Laegemiddel middel = new Laegemiddel("Acetylsalicylsyre", 0.1, 0.15, 0.16, "Styk");
-        test.setLaegemiddel(middel);
         // Act
         boolean aktueltOutput = test.givDosis(givenDate);
         //Assert
