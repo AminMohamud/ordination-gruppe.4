89615a09d2782ce0b4cfedc4d982bc5566f770f8
silasskovchristensen <silasskovchristensen@gmail.com>
2023-02-16 12:29:00 +0100

    ordination ændringer

--- JUnit Tests/src/OrdinationTest/PNTest.java
+++ JUnit Tests/src/OrdinationTest/PNTest.java
@@ -13,8 +13,8 @@ class PNTest {
     void givDosisTC1() {
         //Arrange
         Patient subject = new Patient("111111-1111", "Anders And", 80.00);
-        PN test = new PN(LocalDate.of(1, 2,23), LocalDate.of(12,2,23), 1.00, subject);
-        LocalDate givenDate = LocalDate.of(2,2,23);
+        PN test = new PN(LocalDate.of(23, 2,1), LocalDate.of(23,2,10), 1.00, subject);
+        LocalDate givenDate = LocalDate.of(23,2,1);
         // Act
         boolean aktueltOutput = test.givDosis(givenDate);
         //Assert
@@ -26,8 +26,60 @@ class PNTest {
     void givDosisTC2() {
         //Arrange
         Patient subject = new Patient("111111-1111", "Anders And", 80.00);
-        PN test = new PN(LocalDate.of(1, 2,23), LocalDate.of(12,2,23), 1.00, subject);
-        LocalDate givenDate = LocalDate.of(2,2,23);
+        PN test = new PN(LocalDate.of(23, 2,1), LocalDate.of(23,2,10), 1.00, subject);
+        LocalDate givenDate = LocalDate.of(23,2,7);
+        // Act
+        boolean aktueltOutput = test.givDosis(givenDate);
+        //Assert
+        boolean forventetSvar = true;
+        assertSame(forventetSvar, aktueltOutput);
+    }
+
+    @Test
+    void givDosisTC3() {
+        //Arrange
+        Patient subject = new Patient("111111-1111", "Anders And", 80.00);
+        PN test = new PN(LocalDate.of(23, 2,1), LocalDate.of(23,2,10), 1.00, subject);
+        LocalDate givenDate = LocalDate.of(23,2,10);
+        // Act
+        boolean aktueltOutput = test.givDosis(givenDate);
+        //Assert
+        boolean forventetSvar = true;
+        assertSame(forventetSvar, aktueltOutput);
+    }
+
+    @Test
+    void givDosisTC4() {
+        //Arrange
+        Patient subject = new Patient("111111-1111", "Anders And", 80.00);
+        PN test = new PN(LocalDate.of(23, 2,1), LocalDate.of(23,2,10), 1.00, subject);
+        LocalDate givenDate = LocalDate.of(23,2,11);
+        // Act
+        boolean aktueltOutput = test.givDosis(givenDate);
+        //Assert
+        boolean forventetSvar = false;
+        assertSame(forventetSvar, aktueltOutput);
+    }
+
+    @Test
+    void givDosisTC5() {
+        //Arrange
+        Patient subject = new Patient("111111-1111", "Anders And", 80.00);
+        PN test = new PN(LocalDate.of(23, 1,17), LocalDate.of(23,2,10), 1.00, subject);
+        LocalDate givenDate = LocalDate.of(23,1,32);
+        // Act
+        boolean aktueltOutput = test.givDosis(givenDate);
+        //Assert
+        boolean forventetSvar = true;
+        assertSame(forventetSvar, aktueltOutput);
+    }
+
+    @Test
+    void givDosisTC6() {
+        //Arrange
+        Patient subject = new Patient("111111-1111", "Anders And", 80.00);
+        PN test = new PN(LocalDate.of(23, 2,1), LocalDate.of(23,2,10), 1.00, subject);
+        LocalDate givenDate = null;
         // Act
         boolean aktueltOutput = test.givDosis(givenDate);
         //Assert
